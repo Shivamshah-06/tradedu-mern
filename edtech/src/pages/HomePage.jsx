36b865809d11d6f6b633c54708cdbe6b320b015b
Shivamshah-06 <shivamshah93865@gmail.com>
2024-04-16 00:23:58 +0530

    Init the project

---
+++ edtech/src/pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// import '../style/HomePage.css';
+// import ContactUsForm from '../components/ContactUsForm';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/js/dist/util";
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/dist/js/bootstrap";
+import Slideshow from '../components/SlideShow';
+// import Feedback from 'react-bootstrap/esm/Feedback';
+import Hero from '../components/Hero';
+//import logo from "./logo.jpeg";
+// import Footer from '../components/Footer';
+
+const HomePage = () => {
+  return (
+    <>
+    <Hero/>
+    <h1 className=' fw-bolder 'style={{fontSize:"50px"}}>OUR GALLARY</h1>
+    <Slideshow/>
+    
+    
+    </>
+  );
+}
+
+export default HomePage;
